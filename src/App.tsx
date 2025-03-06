7b56d078c3969dd62a7160adf487a6141e75a3c6
deepak <dnegi1811@gmail.com>
2025-03-06 19:11:16 +0530

    fixes in progressbar

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,14 @@ import ProgressBar from './components/ProgressBar'
 interface ProgressBarData {
   progress: number;
   id: number;
+  show: boolean;
 }
-
-let id = 0;
 
 function App() {
   const [data, setData] = useState<ProgressBarData[]>([])
 
   const addProgressBar = ():void => {
-    setData([...data, {progress: 0, id: id++}]);
+    setData([...data, {progress: 0, id: Date.now(), show: true}]);
   }
 
   return (
@@ -25,12 +24,16 @@ function App() {
       </nav>
       <main className='max-w-screen-xl mx-auto p-4 mt-20 justify-center flex flex-wrap'>
         <button
-        className='text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700'
-        onClick={addProgressBar}
-        >Add Progress Bar</button>
+          className='text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700'
+          onClick={addProgressBar}
+        >
+          Add Progress Bar
+        </button>
 
         {data && data.map((item) => (
-          <ProgressBar key={item.id} progress={item.progress}/>
+          <>
+            {item.show && <ProgressBar key={item.id} progress={item.progress}/>}
+          </>
         ))}
       </main>
       
--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -15,18 +15,26 @@ function ProgressBar({ progress }: ProgressBarProps) {
             }
             setProgressAnimate((prev) => Math.min(100, Math.max(prev + 1, 0)));
         }, 20);
+
+        return () => {
+          clearInterval(interval);
+        }
     }, [progress]);
 
   return (
-    <div className='progress-bar'>
-        <div className='progress-bar__filler' style={{ width: `${progressAnimate}%` }}
-            role="progressbar"
-            aria-valuemin={0}
-            aria-valuemax={100}
-            aria-valuenow={progressAnimate}>
-            <span className='progress-bar__label' style={{color: progressAnimate > 50? "white": "black"}}>{progressAnimate}%</span>
-        </div>
-    </div>
+    <>
+      <div className='progress-bar'>
+          <div className='progress-bar__filler' style={{ width: `${progressAnimate}%` }}
+              role="progressbar"
+              aria-valuemin={0}
+              aria-valuemax={100}
+              aria-valuenow={progressAnimate}>
+              <span className='progress-bar__label' style={{color: progressAnimate > 50? "white": "black"}}>{progressAnimate}%</span>
+          </div>
+      </div>
+    </>
+    
+
   )
 }
 
